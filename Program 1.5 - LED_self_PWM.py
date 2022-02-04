650aa0e90636c37c7cd16426258bb790ca43ed3e
Erwin Fuhrer <88375431+ErwinFuhrer@users.noreply.github.com>
2022-02-04 16:26:49 +0530

    Add files via upload

---
+++ Program 1.5 - LED_self_PWM.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as gpio
+import time
+
+
+#Define the pin we want to control
+LED_PIN = 17
+#Enables to label using GPIO numbers instead of pin numbers
+gpio.setmode(gpio.BCM)
+#Define the controlled pin as output
+gpio.setup(LED_PIN, gpio.OUT)
+
+#defines the frequency of one PWM cycle
+#Change the frequency logarithmically from 1 Hz to 10 kHz 
+freq = 10
+
+try:
+    while True:
+    
+        gpio.output(LED_PIN, gpio.HIGH)
+        time.sleep((1/freq)/2)
+        gpio.output(LED_PIN, gpio.LOW)
+        time.sleep((1/freq)/2)
+
+except KeyboardInterrupt:
+    print('Exit program')
+
+finally:
+    gpio.output(17,gpio.LOW)
+    gpio.cleanup()
+
+
+
+
+
+
---
+++ Program 1.6 - LED_RGB_PWM.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import time
+#import threading as th
+
+
+#Defines the RGB pins
+LED_PIN_R = 17
+LED_PIN_G = 27
+LED_PIN_B = 22
+
+#Enables to label using GPIO numbers instead of pin numbers
+GPIO.setmode(GPIO.BCM)
+
+#Define the controlled pin as output
+GPIO.setup(LED_PIN_R, GPIO.OUT)
+GPIO.setup(LED_PIN_G, GPIO.OUT)
+GPIO.setup(LED_PIN_B, GPIO.OUT)
+
+#defines the frequency of one PWM cycle
+freq = 1000000
+
+#Define 8bit RGB level of individual colors
+R = 125
+G = 255
+B = 255
+
+#Scaling of duty cycle to a number between 0 and 100
+dutycycle_R = 100*R/255
+dutycycle_G = 100*G/255
+dutycycle_B = 100*B/255
+
+#Initiate red LED
+soft_pwm_R = GPIO.PWM(LED_PIN_R,freq)
+soft_pwm_R.start(dutycycle_R)
+
+#Initiate green LED
+soft_pwm_G = GPIO.PWM(LED_PIN_G,freq)
+soft_pwm_G.start(dutycycle_G) 
+
+#Initiate blue LED
+soft_pwm_B = GPIO.PWM(LED_PIN_B,freq)
+soft_pwm_B.start(dutycycle_B)
+
+#Wait for keyboard input to stop programm
+print('Press any key to exit the program')
+input()
+
+
+soft_pwm_R.stop()
+soft_pwm_G.stop()
+soft_pwm_B.stop()
+
+#Shutdown all ports 
+GPIO.cleanup()
+
+
