f96b8bda08ee47ba504b80e29dffdb1940e8863c
Erwin Fuhrer <88375431+ErwinFuhrer@users.noreply.github.com>
2022-02-09 16:03:23 +0530

    Update Program 1.7 - Hardware_PWM.py

--- Program 1.7 - Hardware_PWM.py
+++ Program 1.7 - Hardware_PWM.py
@@ -1,20 +1,24 @@
-import pigpio
-import time
-#Before running this programm: Enable pigpiod daemeon by executing command "sudo pigpiod"
+#Before running this programm: Enable pigpiod daemeon bey executing command "sudo pigpiod"
 #Hardware PWM enables accuracy to a microsecond
+import pigpio
 pi = pigpio.pi()
 
-frequency = 100000
+#set Pin (pigpio uses BCM labeling)
+pin = 13
+
+#Define PWM wave parameters
+frequency = 100
 dutycycpercent = 50#1E-6
 dutycyc = int(dutycycpercent/100*1E6)
 
 #Hardware pins are initiaited through Broadcom number
-pi.hardware_PWM(18, frequency,dutycyc )
+pi.hardware_PWM(pin, frequency,dutycyc )
 
 #Wait for keyboard input to stop programm
 print('Press any key to exit the program')
 input()
-pi.hardware_PWM(18, 0, 0)
+pi.hardware_PWM(pin, 0, 0)
+
 
 
 
